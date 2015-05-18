175ad25b24ab49955d009cf4ca7dbe5c1adfb82c
HenryPi <henryj.clarkson@gmail.com>
2015-05-18 15:26:47 +0800

    JS and CSS more
    YAY for stuff not working

--- JS/setRequired.js
+++ JS/setRequired.js
@@ -11,10 +11,6 @@ function requireNone()
 		change('Triage05L', 'normal');
 		change('Triage06L', 'normal');
 		change('Triage07L', 'normal');
-		change('TriageJustL', 'normal');
-		change('Q1input01L', 'normal');
-		change('Q2select01L', 'normal');
-		change('Q2input01L', 'normal');
 	}
 }
 
@@ -28,21 +24,6 @@ function requireAll() {
 		change('Triage05L', 'required');
 		change('Triage06L', 'required');
 		change('Triage07L', 'required');
-		change('TriageJustL', 'required');
-		change('Q1input01L', 'required');
-		change('Q2select01L', 'required');
-		change('Q2input01L', 'required');
-	}
-}
-
-function triageJustUD() {
-	if((getCheckedValue(document.forms['AppForm'].elements['Triage01']) == "Yes") || (getCheckedValue(document.forms['AppForm'].elements['Triage02']) == "Yes") || (getCheckedValue(document.forms['AppForm'].elements['Triage03']) == "Yes") || (getCheckedValue(document.forms['AppForm'].elements['Triage04']) == "Yes") || (getCheckedValue(document.forms['AppForm'].elements['Triage05']) == "Yes") || (getCheckedValue(document.forms['AppForm'].elements['Triage06']) == "Yes") || (getCheckedValue(document.forms['AppForm'].elements['Triage07']) == "Yes"))
-	{
-		change('TriageJustL', 'required');
-	}
-	else
-	{
-		change('TriageJustL', 'normal');	
 	}
 }
 
